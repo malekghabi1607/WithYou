5e686ad1711ecbce25927b0837e779c30a4cecaf
malek <malek@MacBook-Pro-de-malek.local>
2025-12-04 12:41:46 +0100

    Mise à jour des fichiers de configuration pour supporter TypeScript + Tailwind + nouvelle structure

---
+++ client/tailwind.config.js
@@ -0,0 +1,13 @@
+/**
+ * Projet : WithYou – Plateforme de visionnage collaboratif
+ * Fichier : tailwind.config.js
+ *
+ * Description :
+ * Fichier de configuration de Tailwind CSS.
+ * Sert à :
+ *    - Définir les chemins où Tailwind doit scanner les classes
+ *    - Personnaliser le thème (couleurs, typos…)
+ *    - Ajouter des plugins Tailwind si nécessaire
+ *
+ * Indispensable pour l’utilisation correcte de Tailwind dans le projet.
+ */
