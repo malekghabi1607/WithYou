ca575efb69156146708f8b02189e3bc381a5a719
Wissame11 <taleb.wissame2004@gmail.com>
2025-12-11 19:30:52 +0100

    chore: configuration des channels websocket pour salons

--- server/routes/channels.php
+++ server/routes/channels.php
@@ -5,3 +5,6 @@ use Illuminate\Support\Facades\Broadcast;
 Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
 });
+Broadcast::channel('salon.{id}', function ($user, $id) {
+    return true; // plus tard on peut ajouter sécurité
+});
