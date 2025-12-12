0addd784bdf5ac11aea4af7f2eb18e598e4bdb7e
Meriem <meriemtakdjerad44@gmail.com>
2025-12-12 12:37:51 +0100

    Nettoyage du code : remise à zéro du frontend afin de redémarrer le développement proprement en binôme

--- client/src/api/index.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Projet : WithYou
- * Fichier : api/index.ts
- *
- * Description :
- * Point d'entrée principal pour les modules API.
- * Regroupe et exporte toutes les fonctions d'API afin de
- * centraliser les imports dans l’application.
- */
