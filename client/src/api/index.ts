22a6fcbaf540c4e5ae6d9baec30743913b1dff54
malek <malek@MacBook-Pro-de-malek.local>
2025-12-12 21:50:19 +0100

    Add frontend architecture (src folders)

---
+++ client/src/api/index.ts
@@ -0,0 +1,9 @@
+/**
+ * Projet : WithYou
+ * Fichier : api/index.ts
+ *
+ * Description :
+ * Point d'entrée principal pour les modules API.
+ * Regroupe et exporte toutes les fonctions d'API afin de
+ * centraliser les imports dans l’application.
+ */
