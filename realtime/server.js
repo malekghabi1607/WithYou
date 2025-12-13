692e281faa404c96d74c0dc17fe3d483027954e6
Wissame11 <taleb.wissame2004@gmail.com>
2025-12-14 00:04:57 +0100

    creation de model chatsync.php

---
+++ realtime/server.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+    }
+});
+
+console.log("🚀 Serveur Socket.io démarré");
+
+io.on("connection", (socket) => {
+
+    socket.on("join_salon", (salonId) => {
+        socket.join(salonId);
+        console.log("👤 Un client a rejoint :", salonId);
+    });
+
+    socket.on("video_action", (data) => {
+        console.log("🎬 Action vidéo reçue :", data);
+
+        io.to(data.salon).emit("video_update", {
+            action: data.action,
+            videoId: data.videoId,
+            time: data.time
+        });
+    });
+
+    socket.on("disconnect", () => {
+        console.log("❌ Client déconnecté");
+    });
+});
+
+server.listen(3000, () => {
+    console.log("🔥 Socket.io écoute sur http://localhost:3000");
+});
