eeb9fe72169ebeaef1acc9ad65344394d9edfafd
Meriem <meriemtakdjerad44@gmail.com>
2025-12-20 21:26:26 +0100

    Ajout du composant Avatar

---
+++ client/src/components/ui/avatar.tsx
@@ -0,0 +1,60 @@
+/**
+ * Composant Avatar permettant d’afficher une image de profil.
+ * Inclut une image principale et un fallback en cas d’erreur.
+ * Basé sur Radix UI pour une meilleure accessibilité.
+ * Stylisé avec Tailwind CSS.
+ * Utilisable côté client (Next.js).
+ */
+"use client";
+
+import * as React from "react";
+import * as AvatarPrimitive from "@radix-ui/react-avatar";
+
+import { cn } from "./utils";
+
+function Avatar({
+  className,
+  ...props
+}: React.ComponentProps<typeof AvatarPrimitive.Root>) {
+  return (
+    <AvatarPrimitive.Root
+      data-slot="avatar"
+      className={cn(
+        "relative flex size-10 shrink-0 overflow-hidden rounded-full",
+        className,
+      )}
+      {...props}
+    />
+  );
+}
+
+function AvatarImage({
+  className,
+  ...props
+}: React.ComponentProps<typeof AvatarPrimitive.Image>) {
+  return (
+    <AvatarPrimitive.Image
+      data-slot="avatar-image"
+      className={cn("aspect-square size-full", className)}
+      {...props}
+    />
+  );
+}
+
+function AvatarFallback({
+  className,
+  ...props
+}: React.ComponentProps<typeof AvatarPrimitive.Fallback>) {
+  return (
+    <AvatarPrimitive.Fallback
+      data-slot="avatar-fallback"
+      className={cn(
+        "bg-muted flex size-full items-center justify-center rounded-full",
+        className,
+      )}
+      {...props}
+    />
+  );
+}
+
+export { Avatar, AvatarImage, AvatarFallback };
