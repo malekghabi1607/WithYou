ae36a395298e933f5b581f277ade09679f4c4409
Wissame11 <taleb.wissame2004@gmail.com>
2026-01-01 13:41:29 +0100

    feat: activation du middleware CORS dans bootstrap

--- server/bootstrap/app.php
+++ server/bootstrap/app.php
@@ -13,7 +13,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        // Activer CORS pour toutes les routes API
+        $middleware->api(prepend: [
+            \Illuminate\Http\Middleware\HandleCors::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
