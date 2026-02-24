21ffc13bef394d9858d5ec0576481873b11317ff
malekghabi1607 <145768940+malekghabi1607@users.noreply.github.com>
2026-02-24 14:32:48 +0100

    Configure GitHub Pages deployment for client

--- client/vite.config.js
+++ client/vite.config.js
@@ -17,6 +17,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.GITHUB_ACTIONS ? '/WithYou/' : '/',
   plugins: [react()],
   server: {
     host: true,
